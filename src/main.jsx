e45dc7f5791949866f4719f9bea129edb3de5b19
Maxim Zaikin <mxmzkn19@gmail.com>
2025-11-10 14:45:28 +0200

    Lb9: add layout, cards, useState toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './styles/main.scss'
-import './index.css'
+import './styles/tw.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+            <App/>
+    </StrictMode>,
 )
